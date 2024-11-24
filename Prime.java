2c7fc1143c3d4b146a8162a847c5e2200c847dd1
Alvin Chan <altc.alchan@gmail.com>
2024-11-24 10:51:12 -0500

    Upgrade Prime.java to use BigInteger

--- Prime.java
+++ Prime.java
@@ -1,30 +1,41 @@
+import java.math.BigInteger;
+
 public final class Prime {
-	public static boolean isPrime(int x) {
-		int m = x - 1;
+	final static BigInteger ZERO = new BigInteger("0");
+	final static BigInteger ONE = new BigInteger("1");
+	final static BigInteger TWO = new BigInteger("2");
+
+	private Prime() {}
+
+	public static boolean isPrime(BigInteger x) {
+		if (x.equals(TWO))
+			return true;
+
+		BigInteger m = x.subtract(ONE);
 		for (int k = 1; k < 10; k++) {
-			if ((x - 1) % (1 << k) == 0)
-				m = (x - 1) >> k;
+			if ((x.subtract(ONE)).mod(ONE.shiftLeft(k)).equals(ZERO))
+				m = x.subtract(ONE).shiftRight(k);
 			else
 				break;
 		}
 
-		int a = 2;
+		final BigInteger a = TWO;
+		BigInteger a_m = ONE;
+		for (BigInteger i = ZERO; i.compareTo(m) == -1; i = i.add(ONE))
+			a_m = a_m.multiply(a.multiply(m));
 
-		long a_m = 1;
-		for (int i = 0; i < m; i++)
-			a_m *= a;
-
-		long b = a_m % x;
-		if (b == 1 || b == x-1)
+		BigInteger b = a_m.mod(x);
+		if (b.equals(ONE) || b.equals(x.subtract(ONE)))
 			return true;
 
+		// TODO: up the rounds
 		for (int i = 0; i < 10; i++) {
-			if (b == 1)
+			if (b.equals(ONE))
 				return false;
-			else if (b == x-1)
+			else if (b.equals(x.subtract(ONE)))
 				return true;
 
-			b = (b*b) % x;
+			b = b.modPow(TWO, x);
 		}
 
 		return false;
